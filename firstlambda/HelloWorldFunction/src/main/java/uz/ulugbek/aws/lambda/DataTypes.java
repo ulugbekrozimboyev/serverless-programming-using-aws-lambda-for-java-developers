fa649c3a1b97504e8677e9983869a984e981fbcf
Ulug'bek <ulugbekrozimboyev@gmail.com>
2022-10-26 16:32:59 +0500

    return map without input param

--- firstlambda/HelloWorldFunction/src/main/java/uz/ulugbek/aws/lambda/DataTypes.java
+++ firstlambda/HelloWorldFunction/src/main/java/uz/ulugbek/aws/lambda/DataTypes.java
@@ -30,4 +30,15 @@ public class DataTypes {
     public void saveEmployeeData(Map<String, Integer> map) {
         System.out.println(map);
     }
+
+    public Map<String, List<Integer>> getStudentsScores() {
+        Map<String, List<Integer>> studentScores = new HashMap<>();
+        studentScores.put("Ali", List.of(90, 80, 70));
+        studentScores.put("Vali", List.of(60, 80, 80));
+        studentScores.put("Bali", List.of(50, 85, 75));
+        studentScores.put("Jali", List.of(75, 80, 70));
+        studentScores.put("G'ali", List.of(50, 30, 40));
+
+        return studentScores;
+    }
 }
